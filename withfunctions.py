9e331cc84c2e359a8c7ba92420e9a5b6b4a79df8
G-Nash <20b91a5725@srkrec.ac.in>
2025-04-01 12:52:10 +0530

    funtion based

--- withfunctions.py
+++ withfunctions.py
@@ -1,5 +1,11 @@
 import random
 
+player1=0
+player2=0
+switchTurn=True
+snakes={22:2, 33:3, 44:4, 55:5, 66:6, 77:7, 88:8}
+ladders={11:91, 21:92, 31:93, 41:94, 51:95, 61:96, 71:97, 81:98}
+
 def die():
     return (random.randint(1,6))
 
@@ -17,79 +23,29 @@ def gameMove(player, currentRoll):
         player=ladders[player]
         print("player now at ",player)
         print()
-
-    if currentRoll != 6:
-        switchTurn = not switchTurn
-
-    print("player is at",player)
-    print()
-
-
-player1=0
-player2=0
-switchTurn=True
-
-snakes={22:2, 33:3, 44:4, 55:5, 66:6, 77:7, 88:8}
-ladders={11:91, 21:92, 31:93, 41:94, 51:95, 61:96, 71:97, 81:98}
+    return player
 
 
 while True:
     currentRoll=die()
 
     if switchTurn==True:
-
-        player1 += currentRoll if player1 + currentRoll <= 100 else 0
-
-
-
-        if player1 in snakes:
-            print(f'there is a snake at {player1}')
-            player1=snakes[player1]
-            print("player1 now at ",player1)
-            print()
-
-        if player1 in ladders:
-            print(f'there is a ladder at {player1}')
-            player1=ladders[player1]
-            print("player1 now at ",player1)
-            print()
-
-        if currentRoll != 6:
-            switchTurn = not switchTurn
-
-        print("player1 is at",player1)
+        player1=gameMove(player1,currentRoll)
+        print("player1's at", player1)
         print()
 
+        if player1==100:
+            print("player1's the winner")
+            break
 
     else:
-
-        player2 += currentRoll if player2 + currentRoll <= 100 else 0
-
-
-        if player2 in snakes:
-            print(f'there is a snake at {player2}')
-            player2=snakes[player2]
-            print("player2 now at",player2)
-            print()
-
-        if player2 in ladders:
-            print(f'there is a ladder at {player2}')
-            player2=ladders[player2]
-            print("player2 now at ",player2)
-            print()
-
-        if currentRoll != 6:
-            switchTurn = not switchTurn
-
-        print("player2 is at",player2)
+        player2=gameMove(player2,currentRoll)
+        print("player1's at", player2)
         print()
+
+        if player2==100:
+            print("player2's the winner")
+            break
         
-    
-
-    if player1==100:
-        print("player1's the winner")
-        break
-
-    if player2==100:
-        print("player2's the winner")
-        break
+    if currentRoll != 6:
+        switchTurn = not switchTurn
